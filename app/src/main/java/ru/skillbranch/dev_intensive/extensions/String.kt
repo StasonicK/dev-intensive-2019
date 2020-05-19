4e8c4699336a3866aeec1935b5070944b13d5c53
Stas Anishchenko <anischenko_stas@mail.ru>
2020-05-19 21:49:10 +0500

    -

--- app/src/main/java/ru/skillbranch/dev_intensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/dev_intensive/extensions/String.kt
@@ -1,21 +1,16 @@
 package ru.skillbranch.dev_intensive.extensions
 
 fun String.truncate(length: Int = 16): String {
-    var text = this
+    val text = this
     var result: StringBuilder = StringBuilder()
     val filler = "..."
-    if (text[length - 1] == ' ' && text[length] == ' ') return result.append(text.substring(0, length)).toString()
-        .trim()
 
-    if (text[length - 1] != ' ') {
-        return result.append(text.substring(0, length)).append(filler).toString()
-    } else {
-        text = result.append(text.substring(0, length)).toString().trim()
-        for (s in text.length..0) {
-            if (text[s] == ' ') text = text.substring(0, s)
-        }
+    if (text[length] == ' ') {
+        result.append(text.substring(0, length)).toString().trim()
+    }else{
+        result.append(text.substring(0, length)).append(filler).toString().trim()
     }
-    return text + filler
+    return result.toString()
 }
 
 fun String.stripHtml(): String {
