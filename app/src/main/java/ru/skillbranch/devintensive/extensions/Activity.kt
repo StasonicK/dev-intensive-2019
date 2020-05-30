60a6a55ba4498746b1d19e0c937e9cd7697d2bae
Stas Anishchenko <anischenko_stas@mail.ru>
2020-05-30 18:50:29 +0500

    - finished hometask_3 excepted 2 last methods

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,2 +1,15 @@
 package ru.skillbranch.devintensive.extensions
 
+import android.app.Activity
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.hideKeyboard() {
+    val imm = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    val view = this.currentFocus
+    imm.hideSoftInputFromWindow(view?.windowToken, 0)
+}
+
+
+//TODO finish it!
+fun Activity.isKeyboardOpen() {
+}
--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -1,21 +1,23 @@
 package ru.skillbranch.devintensive.models
 
-class Bander(var status: Status = Status.NORMAL, var question: Questiion = Questiion.NAME) {
+class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
 
     fun askQuestion(): String = when (question) {
-        Questiion.NAME -> Questiion.NAME.question
-        Questiion.PROFESSION -> Questiion.PROFESSION.question
-        Questiion.MATERIAL -> Questiion.MATERIAL.question
-        Questiion.BDAY -> Questiion.BDAY.question
-        Questiion.SERIAL -> Questiion.SERIAL.question
-        Questiion.IDLE -> Questiion.IDLE.question
+        Question.NAME -> Question.NAME.question
+        Question.PROFESSION -> Question.PROFESSION.question
+        Question.MATERIAL -> Question.MATERIAL.question
+        Question.BDAY -> Question.BDAY.question
+        Question.SERIAL -> Question.SERIAL.question
+        Question.IDLE -> Question.IDLE.question
     }
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
         return if (question.answers.contains(answer)) {
-            "Отлично - это правильный ответ!" to status.color
+            question = question.nextQuestion()
+            "Отлично - это правильный ответ!\n${question.question}" to status.color
         } else {
-            "Отлично - это правильный ответ!" to status.color
+            status = status.nextStatus()
+            "Это неправильный ответ!\n${question.question}" to status.color
         }
     }
 
@@ -23,15 +25,37 @@ class Bander(var status: Status = Status.NORMAL, var question: Questiion = Quest
         NORMAL(Triple(255, 255, 255)),
         WARNING(Triple(255, 120, 0)),
         DANGER(Triple(255, 60, 60)),
-        CRITICAL(Triple(255, 255, 0))
+        CRITICAL(Triple(255, 255, 0));
+
+        fun nextStatus(): Status {
+            return if (this.ordinal < values().lastIndex) {
+                values()[this.ordinal + 1]
+            } else {
+                values()[0]
+            }
+        }
     }
 
-    enum class Questiion(val question: String, val answers: List<String>) {
-        NAME("Как меня зовут?", listOf("бендер", "bender")),
-        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")),
-        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")),
-        BDAY("Когда меня создали?", listOf("2993")),
-        SERIAL("Какой мой серийный номер?", listOf("2716057")),
-        IDLE("На этом всё, вопросов больше нет", listOf())
+    enum class Question(val question: String, val answers: List<String>) {
+        NAME("Как меня зовут?", listOf("бендер", "bender")) {
+            override fun nextQuestion(): Question = PROFESSION
+        },
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
+            override fun nextQuestion(): Question = MATERIAL
+        },
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
+            override fun nextQuestion(): Question = BDAY
+        },
+        BDAY("Когда меня создали?", listOf("2993")) {
+            override fun nextQuestion(): Question = SERIAL
+        },
+        SERIAL("Какой мой серийный номер?", listOf("2716057")) {
+            override fun nextQuestion(): Question = IDLE
+        },
+        IDLE("На этом всё, вопросов больше нет", listOf()) {
+            override fun nextQuestion(): Question = IDLE
+        };
+
+        abstract fun nextQuestion(): Question
     }
 }
